988033d20ae58e9a99bfb7d346042ac260d9795d
Cail Young <cailyoung@Clios-MacBook-Pro.local>
2019-08-01 01:39:41 +1000

    search list of accounts for the right one

--- selenium-tests/test.cs
+++ selenium-tests/test.cs
@@ -5,7 +5,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
-
+using System.Collections.Generic;
 using System;
 
 namespace InterviewDemo
@@ -39,12 +39,18 @@ namespace InterviewDemo
         {
             _webDriver.Navigate().GoToUrl("https://go.xero.com/Banking/Account/#find");
             var wait = new WebDriverWait(_webDriver, new TimeSpan(0, 0, 30));
-            IWebElement element = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector("input[data-ref='inputEl']")));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector("input[data-ref='inputEl']")));
             _webDriver.FindElement(By.CssSelector("input[data-ref='inputEl']")).SendKeys("ANZ (NZ)");
-            IWebElement element2 = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.ClassName("ba-allbanks-filter-prompt")));
-            // yep
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.ClassName("ba-allbanks-filter-prompt")));
             _webDriver.FindElement(By.ClassName("ba-allbanks-filter-prompt")).Click();
-            Assert.True(true);
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("#ba-banklist-1023 ul li")));
+            var bankAccountElements = _webDriver.FindElements(By.CssSelector("#ba-banklist-1023 ul li"));
+            var accountNames = new List<string>();
+            foreach (IWebElement bankAccount in bankAccountElements) {
+                string accountName = bankAccount.GetProperty("innerText");
+                accountNames.Add(accountName);
+            }
+            Assert.Contains("ANZ (NZ)",accountNames);
         }
     }
 }
